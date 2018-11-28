2f56a5c595e461c0bfddd9c5ac7b41915f5213b0
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-28 09:14:17 +0600

    updated config generation with tests
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- config/config.go
+++ config/config.go
@@ -1,9 +1,38 @@
+/*
+Example config.json
+{
+    "Port": ":8081"
+}
+*/
+
 package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
 
 type Config struct {
 	Port string
 }
 
-func Get() (*Config, error) {
-	return &Config{Port: "12345"}, nil
+var err error
+var config Config
+
+func Get(filename string) (Config, error) {
+	configFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Print("Unable to read config file, switching to flag mode")
+		return Config{}, err
+	}
+
+	err = json.Unmarshal(configFile, &config)
+	if err != nil {
+		log.Print("Invalid JSON, expecting port from command line flag.")
+		return Config{}, err
+	}
+
+	return config, nil
+
 }
