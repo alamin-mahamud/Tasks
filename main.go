ae056c230f742f8bf559bae23eeb762c0b8fa035
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-29 01:23:17 +0600

    finished reading cii flag for port
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- main.go
+++ main.go
@@ -16,6 +16,13 @@ func main() {
 	// 1. get configs and override with cli flags
 	values, err := config.Get("config/config.json")
 	checkError(err, "Could Not Parse Port & Other Configuration")
+
+	// get port from env if config is not present
+	if err != nil {
+		port := config.GetPortFromCli()
+		values.Port = port
+	}
+
 	fmt.Println("Hello " + values.Port)
 
 	// 2. register routes
