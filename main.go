f2d3613a7d4028b6d2e0252939c66a2f438464ea
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-28 07:29:37 +0600

    update main.go
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/alamin-mahamud/go-tasks/config"
+)
+
+func checkError(err error, msg string) {
+	if err != nil {
+		fmt.Println(msg)
+	}
+}
+
 func main() {
 	// 1. get configs and override with cli flags
+	values, err := config.Get()
+	checkError(err, "Could Not Parse Port & Other Configuration")
+	fmt.Println(values.Port)
 
 	// 2. register routes
 
 	// 3. start server
-
 }
